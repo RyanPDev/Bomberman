cebc39b058a180c0c38c9477e0e2d6cb46437664
Ryan Palazón <ryan.palazon@enti.cat>
2021-01-21 22:09:42 +0100

    release push

--- src/AA2_Bomberman/Ranking.cpp
+++ src/AA2_Bomberman/Ranking.cpp
@@ -27,10 +27,10 @@ Ranking::Ranking()
 	Renderer::GetInstance()->LoadRect(T_NAME, { SCREEN_WIDTH / 2 - vtmp.x / 2 + 200, 150, vtmp.x, vtmp.y });
 
 	//Back Button 
-	vtmp = Renderer::GetInstance()->LoadTextureText(F_GAMEOVER, Text{ T_BACK_N, "EXIT", {0,0,0,255}, 0, 0 });
-	Renderer::GetInstance()->LoadTextureText(F_GAMEOVER, Text{ T_BACK_H, "EXIT", {200,100,0,255}, 0, 0 });
+	vtmp = Renderer::GetInstance()->LoadTextureText(F_GAMEOVER, Text{ T_BACK_N, "RETURN TO MAIN MENU", {250,128,114,255}, 0, 0 });
+	Renderer::GetInstance()->LoadTextureText(F_GAMEOVER, Text{ T_BACK_H, "RETURN TO MAIN MENU", {220,220,220,255}, 0, 0 });
 	textureBackToMenu = T_BACK_N;
-	Renderer::GetInstance()->LoadRect(T_BACK, { 560, 580, vtmp.x, vtmp.y });
+	Renderer::GetInstance()->LoadRect(T_BACK, { SCREEN_WIDTH / 2 - vtmp.x / 2, 580, vtmp.x, vtmp.y });
 
 	ReadFile();
 }
@@ -72,13 +72,19 @@ void Ranking::Draw()
 	//RANK, SCORE AND NAME VALUES FOR EACH PLAYER IN THE RANKING
 	for (int i = 0; i < rank.size() && i < 10; i++)
 	{
-		VEC2 vtmp = Renderer::GetInstance()->UpdateTextureText(F_GAMEOVER, Text{ T_PL_NAMES, rank[i].name, {0, 0, 0, 255}, 0, 0 });
+		Color color = {0, 0, 0, 0};
+		if (i == 0) color = { 255, 215, 0, 255 };
+		else if (i == 1) color = { 211, 211, 211, 255 };
+		else if (i == 2) color = { 205, 127, 50, 255 };
+		else color = { 0, 0, 0, 255 };
+
+		VEC2 vtmp = Renderer::GetInstance()->UpdateTextureText(F_GAMEOVER, Text{ T_PL_NAMES, rank[i].name, color, 0, 0 });
 		Renderer::GetInstance()->UpdateRect(T_PL_NAMES, { SCREEN_WIDTH / 2 - vtmp.x / 2 + 200, 200 + (35 * i), vtmp.x, vtmp.y });
 		Renderer::GetInstance()->PushImage(T_PL_NAMES, T_PL_NAMES);
-		vtmp = Renderer::GetInstance()->UpdateTextureText(F_GAMEOVER, Text{ T_PL_SCORES, std::to_string(rank[i].score), {0, 0, 0, 255}, 0, 0 });
+		vtmp = Renderer::GetInstance()->UpdateTextureText(F_GAMEOVER, Text{ T_PL_SCORES, std::to_string(rank[i].score), color, 0, 0 });
 		Renderer::GetInstance()->UpdateRect(T_PL_SCORES, { SCREEN_WIDTH / 2 - vtmp.x / 2, 200 + (35 * i), vtmp.x, vtmp.y });
 		Renderer::GetInstance()->PushImage(T_PL_SCORES, T_PL_SCORES);
-		vtmp = Renderer::GetInstance()->UpdateTextureText(F_GAMEOVER, Text{ T_PL_RANK, std::to_string(i + 1), {0, 0, 0, 255}, 0, 0 });
+		vtmp = Renderer::GetInstance()->UpdateTextureText(F_GAMEOVER, Text{ T_PL_RANK, std::to_string(i + 1), color, 0, 0 });
 		Renderer::GetInstance()->UpdateRect(T_PL_RANK, { SCREEN_WIDTH / 2 - vtmp.x / 2 - 200, 200 + (35 * i), vtmp.x, vtmp.y });
 		Renderer::GetInstance()->PushImage(T_PL_RANK, T_PL_RANK);
 	}
